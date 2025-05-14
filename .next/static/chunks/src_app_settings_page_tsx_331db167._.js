7336a8dea251f4359f3e22b3186507d502bb844d
Willtanoe <willtanu86@gmail.com>
2025-05-14 12:27:40 +0700

    Add .next/static/chunks/src_app_settings_page_tsx_331db167._.js 1747200460242

---
+++ .next/static/chunks/src_app_settings_page_tsx_331db167._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_27ad8fe2._.js"
+],
+    source: "dynamic"
+});
